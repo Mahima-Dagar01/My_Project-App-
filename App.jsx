c2c49371891eb20c10ba476ee153fe4d9552851f
Mahima-Dagar01 <dagarmahima002@gmail.com>
2025-09-03 15:20:06 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text, Button} from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>App   This is my love
+      </Text>
+      <Button title='Press here'/>
+    </View>
+  )
+}
+
+export default App
